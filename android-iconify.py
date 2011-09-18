323975cbb11be76f552aa7f8d879969cc91b26ed
Michael J. Mitchell <michael@mitchtech.net>
2011-09-18 15:32:17 -0400

    script expanded to generate res/ directory and sub directories

--- android-iconify.py
+++ android-iconify.py
@@ -1,24 +1,55 @@
 #!/usr/bin/python
+"""
+@file android_iconify.py
+@author Michael Mitchell
+@date Sept 18, 2011
+@brief Automate the re-sizing of icons for Android using python and ImageMagick.
+@usage android-iconify.py icon_file_to_convert.png
+
+Naming and sizing based on suggested developer practices here:
+
+http://developer.android.com/guide/practices/ui_guidelines/icon_design.html
+"""
 import os, sys
 
-if len(sys.argv) != 2:  # the program name and image
-  # stop the program and print an error message
+if len(sys.argv) != 2:  # the program name and image file.png
   sys.exit("Usage: android_iconify.py icon_file_to_convert.png")
 
 file = sys.argv[1]
 if file.endswith('.png'):
-	new = (os.path.splitext(file)[0] +  '_72.png')
-	os.system ("convert %s -resize 72x72 %s" % (file, new))
-	new = (os.path.splitext(file)[0] +  '_64.png')
-	os.system ("convert %s -resize 64x64 %s" % (file, new))
-	new = (os.path.splitext(file)[0] +  '_48.png')
-	os.system ("convert %s -resize 48x48 %s" % (file, new))
-	new = (os.path.splitext(file)[0] +  '_36.png')
+	# create directory structure
+	newdir = ('./res')
+	os.mkdir(newdir)
+	newdir = ('./res/drawable-ldpi')
+	os.mkdir(newdir)
+	newdir = ('./res/drawable-mdpi')
+	os.mkdir(newdir)
+	newdir = ('./res/drawable-hdpi')
+	os.mkdir(newdir)
+	# drawable-ldpi
+	new = ('./res/drawable-ldpi/ic_launcher_' + file)
 	os.system ("convert %s -resize 36x36 %s" % (file, new))
-	new = (os.path.splitext(file)[0] +  '_32.png')
-	os.system ("convert %s -resize 32x32 %s" % (file, new))
-	new = (os.path.splitext(file)[0] +  '_24.png')
+	new = ('./res/drawable-ldpi/ic_stat_notify_' + file)
+	os.system ("convert %s -resize 19x19 %s" % (file, new))
+	new = ('./res/drawable-ldpi/ic_dialog_' + file)
 	os.system ("convert %s -resize 24x24 %s" % (file, new))
-
+	# drawable-mdpi
+	new = ('./res/drawable-mdpi/ic_launcher_' + file)
+	os.system ("convert %s -resize 48x48 %s" % (file, new))
+	new = ('./res/drawable-mdpi/ic_stat_notify_' + file)
+	os.system ("convert %s -resize 25x25 %s" % (file, new))
+	new = ('./res/drawable-mdpi/ic_dialog_' + file)
+	os.system ("convert %s -resize 24x24 %s" % (file, new))
+	# drawable-hdpi
+	new = ('./res/drawable-hdpi/ic_launcher_' + file)
+	os.system ("convert %s -resize 36x36 %s" % (file, new))
+	new = ('./res/drawable-hdpi/ic_stat_notify_' + file)
+	os.system ("convert %s -resize 19x19 %s" % (file, new))
+	new = ('./res/drawable-hdpi/ic_dialog_' + file)
+	os.system ("convert %s -resize 24x24 %s" % (file, new))
+	
 print "Complete"
+
+
+
 
